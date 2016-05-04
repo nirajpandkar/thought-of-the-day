1eb3cc5a2b1dc7cc48f09c120889d673ce69f45c
nirajpandkar <niraj.pandkar@gmail.com>
2016-05-04 07:03:06 +0530

    added particular function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 var uniqueRandomArray = require("unique-random-array");
+//var uniqueRandom = require("unique-random");  //for use in future
 var allThoughts = require("./thoughts.json");
 module.exports = {
     random:uniqueRandomArray(allThoughts),
-    all:allThoughts
+    all:allThoughts,
+    particular:particular
 };
+function particular(n){
+    if(n<0 || n>=allThoughts.length)
+        return "Wrong index";
+    else if(n == null)
+        return "Index required";
+    else
+        return allThoughts[n];
+}
