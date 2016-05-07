59bfab3ad42236b9296420bd8f6ef3de5febb4ca
nirajpandkar <niraj.pandkar@gmail.com>
2016-05-07 11:07:51 +0530

    used json api for quotes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var uniqueRandomArray = require("unique-random-array");
-//var uniqueRandom = require("unique-random");  //for use in future
+var request = require('request');
 var allThoughts = require("./thoughts.json");
 module.exports = {
     random:uniqueRandomArray(allThoughts),
     all:allThoughts,
-    particular:particular
+    particular:particular,
+    tod:tod
 };
 function particular(n){
     if(n<0 || n>=allThoughts.length)
@@ -13,4 +14,24 @@ function particular(n){
         return "Index required";
     else
         return allThoughts[n];
+}
+
+function tod(){
+    request('http://quotes.rest/qod.json', function (error, response, body) {
+        //Check for error
+        if(error){
+            return console.log('Error:', error);
+        }
+
+        //Check for right status code
+        if(response.statusCode !== 200){
+            return console.log('Invalid Status Code Returned:', response.statusCode);
+        }
+
+        //All is good. Print the quote
+        var data = JSON.parse(body);
+        console.log(data.contents.quotes[0].quote);
+
+    });
+
 }
