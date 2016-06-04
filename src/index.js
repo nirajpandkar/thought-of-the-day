1cc3ebb62d6cb6d0860c2c6c8e14ddd4ddc6670f
nirajpandkar <niraj.pandkar@gmail.com>
2016-06-04 17:09:30 +0530

    perf(index): added more functionality to random and particular functions
    random and particular functions take 'category' as parameter
    
    closes #8 BREAKING CHANGE: Implementation of 'random' and 'particular' function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 var uniqueRandomArray = require("unique-random-array");
 var allThoughts = require("./thoughts.json");
 module.exports = {
-    random:random(),
+    random:random,
     all:allThoughts,
     particular:particular
 };
-
-function random(){
-    return uniqueRandomArray(allThoughts.startup);
-}
-function particular(n){
-    if(n<0 || n>=allThoughts.startup.length)
-        return "Index out of bound";
-    else if(n == null)
-        return "Index required";
+function random(category){
+    if(category == null) {
+        var randomCategory = uniqueRandomArray(allThoughts.categories);
+        var randomQuote = uniqueRandomArray(allThoughts[randomCategory()]);
+        return randomQuote();
+    }
     else{
-        return allThoughts.startup[n];
+        var quote = uniqueRandomArray(allThoughts[category]);
+        return quote();
     }
 }
+
+function particular(category,n){
+    if(n<0 || n>=allThoughts[category].length)
+        return "Index out of bound";
+    else if(n == null || category == null)
+        return "Either index or category is missing!";
+    else{
+        return allThoughts[category][n];
+    }
+}
+//var lib = require("./src/index.js");lib.particular();
