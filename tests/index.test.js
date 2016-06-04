b0305788e9903f93754e29967d5a2b5b4b854396
nirajpandkar <niraj.pandkar@gmail.com>
2016-06-04 17:29:03 +0530

    test(index.test.js): modified some tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,27 +6,32 @@ describe("thoughts",function(){
     describe("random",function(){
         it("should return an object with random quote and corresponding author",function(){
             var randomQuote = thoughts.random();
-            expect(thoughts.all.startup).to.include(randomQuote);
+            expect(randomQuote).to.be.an('object');
         });
 
         it("should return a different object than the previous one every time",function(){
             var prevRandomQuote = thoughts.random();
             var newRandomQuote = thoughts.random();
             expect(prevRandomQuote).to.be.not.equal(newRandomQuote);
+        });
+
+        it("should return object from specified category",function(){
+            var randomQuote = thoughts.random("anonymous");
+            expect(thoughts.all.anonymous).to.include(randomQuote);
         })
     });
 
     describe("particular",function(){
         it("should return a particular object",function(){
-            var particularQuote = thoughts.particular(3);
+            var particularQuote = thoughts.particular("startup",3);
             expect(thoughts.all.startup[3]).to.be.equal(particularQuote);
         });
 
         it("should return an appropriate warning if correct parameter not given",function(){
-            var wrongIndex = thoughts.particular(-1);
-            var noIndex = thoughts.particular();
-            expect(wrongIndex).to.equal("Index out of bound");
-            expect(noIndex).to.equal("Index required");
+            var wrongIndex = thoughts.particular("startup",-1);
+            var noIndex = thoughts.particular("startup");
+            expect(wrongIndex).to.be.a('string');
+            expect(noIndex).to.be.a('string');
         })
     })
 });
